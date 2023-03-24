1d8ccda289411ffc06f02f5704305ac037d011b6
Adam Zahradník <adam@zahradnik.xyz>
2023-03-25 00:19:51 +0100

    runner: fix observer newlines

--- runner/cmd_observer.go
+++ runner/cmd_observer.go
@@ -3,7 +3,7 @@ package main
 import "github.com/trojsten/ksp-proboj/common"
 
 func cmdToObserver(m *Match, _ []string, payload string) common.RunnerResponse {
-	_, err := m.observer.Write([]byte(payload))
+	_, err := m.observer.Write([]byte(payload + "\n"))
 	if err != nil {
 		return common.RunnerResponse{Status: common.Error}
 	}
